70e0d0a350527df0f1732e1a1c40deef5883ccf0
Henry Baum <henryandrewbaum@gmail.com>
2020-10-22 22:32:31 -0400

    Express Up and Running

---
+++ server.js
@@ -0,0 +1,45 @@
+/*********************
+**** Initializers ****
+*********************/
+
+//Initialize express
+const express = require('express');
+const app = express();
+
+//Reads .env file for PORT number
+require('dotenv').config();
+const port = process.env.PORT || 3000;
+
+//Point to static assets
+app.use(express.static('public'));
+
+//Allows us to recognize JSON, strings, and array objects
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+//Initialize method-override
+const methodOverride = require('method-override');
+app.use(methodOverride('_method'));
+
+//Initialize mongoose with mongoDB
+const mongoURI = process.env.MONGODBURI;
+const mongoose = require('mongoose');
+mongoose.connect(mongoURI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false    
+});
+
+/******************
+**** LISTENERS ****
+*******************/
+
+//mongoose listeners
+mongoose.connection.on('error', (err) => console.log("*** Error connecting to MongoDB ***"));
+mongoose.connection.on('connected', () => console.log("*** MongoDB connected at " + mongoURI + " ***"));
+mongoose.connection.on('disconnected', () => console.log("*** MongoDB disconnected at " + mongoURI + " ***"));
+
+//express listener
+app.listen(port, () => {
+    console.log("*** Express server running at localhost:" + port + " ***");
+});
